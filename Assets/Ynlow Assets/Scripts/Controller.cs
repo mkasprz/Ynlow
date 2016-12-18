2cc50528325f2b49382dda0eb4c36a2607561447
mfurmane <mfurmane9@gmail.com>
2016-12-18 07:39:54 +0100

    Input enablation and disablation

--- Assets/Ynlow Assets/Scripts/Controller.cs
+++ Assets/Ynlow Assets/Scripts/Controller.cs
@@ -13,22 +13,33 @@ public class Controller : MonoBehaviour {
 	public float cooldownCounter = 0;
 	public float mo;
 
+	private bool inputEnabled = true;
 	public float tmp = 0;
 
 	void Awake () {
 
 	}
 
+	public void enableInput() {
+		inputEnabled = true;
+	}
+	public void disableInput() {
+		inputEnabled = false;
+	}
+
 	public void jump() {
-		Debug.Log (GetComponent<Rigidbody2D> ());
-		if (!foots.inTheAir)
-			rigid.velocity = new Vector2 (0, jumpHigh);
-		foots.inTheAir = true;
+		if (inputEnabled) {
+			if (!foots.inTheAir)
+				rigid.velocity = new Vector2 (0, jumpHigh);
+			foots.inTheAir = true;
+		}
 	}
 
 	public void makeMove(Vector3 move) {
 		mo = Mathf.Abs (move.x);
-		rigid.velocity = new Vector2((move * speed*1.5f).x, rigid.velocity.y);
+		if (inputEnabled) {
+			rigid.velocity = new Vector2 ((move * speed * 1.5f).x, rigid.velocity.y);
+		}
 	}
 
 	public void attack() {
